c0dc1cf0f8b6024c4f03ca1a59e2f23c2865492f
rifqifinaldy <rifqifinaldy@gmail.com>
2022-03-30 15:57:58 +0700

    setting up TS

---
+++ sandbox.js
@@ -0,0 +1,7 @@
+var myName = 'Rifqi Finaldy';
+console.log(myName);
+var inputs = document.querySelectorAll('input');
+console.log(inputs);
+inputs.forEach(function (input) {
+    console.log(input);
+});
---
+++ sandbox.ts
@@ -0,0 +1,10 @@
+const myName = 'Rifqi Finaldy';
+
+console.log(myName);
+
+const inputs = document.querySelectorAll('input');
+console.log(inputs);
+
+inputs.forEach(input => {
+    console.log(input);
+})
