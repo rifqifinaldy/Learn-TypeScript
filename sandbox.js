3f86bdb0eb7a795cbfddf753ea259e1ca4024b4c
rifqifinaldy <rifqifinaldy@gmail.com>
2022-03-30 16:30:18 +0700

    Array & Obj

--- sandbox.js
+++ sandbox.js
@@ -8,3 +8,28 @@ var circle = function (diameter) {
     return diameter * Math.PI;
 };
 console.log(circle(7.5));
+// ---- Using Array ----
+// String
+var names = ["Rifqi", "Finaldy", "Lala"];
+names.push("Rifqi2");
+// Mixed
+var mixed = ["ken", 4, true];
+mixed.push("ryu");
+mixed.push(10);
+mixed[0] = 3;
+console.log("Array Case", names, mixed);
+// Object 
+var animal = {
+    name: 'Singa',
+    kind: 'Mamalia',
+    diet: 'carnivore'
+};
+// IT's WORKING
+animal.diet = 'herbivore';
+// IT"S NOT WORKING -> HARUS DEFINE DULU
+// animal.skills = ['hunting', 'climbing'];
+// Harus Sama
+// animal = {
+//     name : 'maung',
+//     kind : 'mamal',
+// }
--- sandbox.ts
+++ sandbox.ts
@@ -11,3 +11,36 @@ const circle = (diameter: number) => {
 };
 
 console.log(circle(7.5));
+
+// ---- Using Array ----
+// String
+let names = ["Rifqi", "Finaldy", "Lala"];
+names.push("Rifqi2");
+
+// Mixed
+let mixed = ["ken", 4, true];
+
+mixed.push("ryu");
+mixed.push(10);
+mixed[0] = 3;
+
+console.log("Array Case", names, mixed);
+
+// Object 
+let animal = {
+    name : 'Singa',
+    kind : 'Mamalia',
+    diet : 'carnivore'
+}
+
+// IT's WORKING
+animal.diet = 'herbivore'; 
+
+// IT"S NOT WORKING -> HARUS DEFINE DULU
+// animal.skills = ['hunting', 'climbing'];
+
+// Harus Sama
+// animal = {
+//     name : 'maung',
+//     kind : 'mamal',
+// }
